2f3b6a612756de9af9c6c3cf9c1100ee60ddf0bf
Marcos Conte Lima <marcoscontelima@yahoo.com.br>
2020-09-20 20:24:04 -0300

    Confirgurando front-web para prod

--- front-web/src/pages/Charts/index.tsx
+++ front-web/src/pages/Charts/index.tsx
@@ -20,7 +20,7 @@ const initialPieData = {
     series: []
 }
 
-const BASE_URL = 'http://localhost:8080'
+const BASE_URL = 'https://sds1-marcos.herokuapp.com';
 
 const Charts = () => {
     const [barChartData, setBarChartData] = useState<BarChartData[]>([]);
